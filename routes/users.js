536f948455eed1ea1d35b0c97181f90456dd7997
Mickael Halimi <halimimickael.halimi@gmail.com>
2025-02-03 22:57:10 +0100

    upgrade bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express= require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { UserModel, userValid, loginValid, createToken } = require("../models/userModel");
 
 const router = express.Router();
